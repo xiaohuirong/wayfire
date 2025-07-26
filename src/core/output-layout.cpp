245e74429935280b87f79ef77107e0e8ff2baa00
hoream <hoream@qq.com>
2025-07-26 19:31:38 +0800

    revert: output-layout: change rotation handling to match other compositors

--- src/core/output-layout.cpp
+++ src/core/output-layout.cpp
@@ -25,32 +25,30 @@ static void*const WF_NOOP_OUTPUT_MAGIC = (void*)0x1234;
 
 static wl_output_transform get_transform_from_string(std::string transform)
 {
-    // Most compositors like Sway and Weston use clockwise rotations.
-    // We try to follow the 'convention'.
     if (transform == "normal")
     {
         return WL_OUTPUT_TRANSFORM_NORMAL;
     } else if (transform == "90")
     {
-        return WL_OUTPUT_TRANSFORM_270;
+        return WL_OUTPUT_TRANSFORM_90;
     } else if (transform == "180")
     {
-        return WL_OUTPUT_TRANSFORM_270;
+        return WL_OUTPUT_TRANSFORM_180;
     } else if (transform == "270")
     {
-        return WL_OUTPUT_TRANSFORM_90;
+        return WL_OUTPUT_TRANSFORM_270;
     } else if (transform == "flipped")
     {
         return WL_OUTPUT_TRANSFORM_FLIPPED;
+    } else if (transform == "90_flipped")
+    {
+        return WL_OUTPUT_TRANSFORM_FLIPPED_90;
     } else if (transform == "180_flipped")
     {
         return WL_OUTPUT_TRANSFORM_FLIPPED_180;
-    } else if (transform == "90_flipped")
-    {
-        return WL_OUTPUT_TRANSFORM_FLIPPED_270;
     } else if (transform == "270_flipped")
     {
-        return WL_OUTPUT_TRANSFORM_FLIPPED_90;
+        return WL_OUTPUT_TRANSFORM_FLIPPED_270;
     }
 
     LOGE("Bad output transform in config: ", transform);
